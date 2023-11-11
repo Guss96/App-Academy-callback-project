82e4a784d3c142b323fe22e4dc9f93dc6ae64885
ghassen <amrighassen14@gmail.com>
2023-11-11 19:21:34 +0300

    prob 2 done

--- problems/02-my-map.js
+++ problems/02-my-map.js
@@ -6,6 +6,15 @@ each element of the array, passing in the element.
 Do not use the built in Array#map
 
 // Examples
+*******************************************************************************/
+
+let myMap = function(array,cb) {
+    let newArr = [];
+    for (i = 0; i < array.length; i++){
+        newArr.push(cb(array[i]));
+    }
+    return newArr;
+};
 
 let result1 = myMap([100, 25, 81, 64], Math.sqrt);
 console.log(result1);   // [ 10, 5, 9, 8 ]
@@ -14,13 +23,6 @@ let result2 = myMap(['run', 'Forrest'], function (el) {
     return el.toUpperCase() + '!';
 });
 console.log(result2);   // [ 'RUN!', 'FORREST!' ]
-*******************************************************************************/
-
-let myMap = function() {
-
-};
-
-
 
 
 
